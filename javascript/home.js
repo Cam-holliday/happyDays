6f77d4b5b0215571189981d7968e13b313fb95bb
Camden Holliday <camdenholliday1@gmail.com>
2021-11-29 20:12:06 -0500

    sidebar

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,22 @@
+document.querySelector(".hamburger-menu").addEventListener(
+    "click", ()=> {
+        document.querySelector(".container").classList.toggle("change")
+    }
+)
+/*
+let i = 1
+document.getElementById("sidebar").style.display = "none"
+function change(){
+    if(i === 1){
+        document.getElementById("sidebar").style.display = "block"
+        i = 0
+    }
+    else{
+        document.getElementById("sidebar").style.display = "none"
+        i = 1
+    }
 
-
-function colorChange(){
-    document.getElementById("h1").style.color = "green";
+    
 }
-document.getElementById("h1").onclick = colorChange
+
+document.getElementById("hamburger-menu").onclick = change*/
