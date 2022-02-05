62c51df4b7bef01f4d9e11892a710c6627337159
Camden Holliday <camdenholliday1@gmail.com>
2022-02-05 09:40:09 -0500

    zoom-in and out for img's

--- javascript/home.js
+++ javascript/home.js
@@ -19,9 +19,97 @@ function change(){
     
 }
 
-document.getElementById("hamburger-menu").onclick = change*/
+document.getElementById("hamburger-menu").onclick = change
 
 function fullImage(){
     document.getElementById("img").style.width = "500px";
 
 }
+let button = document.getElementById("button");
+let tatimg = document.getElementById("img1");
+let trans = setInterval(frame, 5);
+function zoomIn(){
+    
+    let currWidth = tatimg.clientWidth;
+    let currHeight = tatimg.clientHeight;
+    tatimg.style.width = (currWidth + 500) + "px";
+    tatimg.style.height = (currHeight + 350) + "px";
+}
+
+
+*/
+
+document.querySelector(".img-box1").addEventListener("click", ()=>{
+    document.querySelector(".img1").classList.toggle("trans")
+});
+
+document.querySelector(".img-box2").addEventListener("click", ()=>{
+    document.querySelector(".img2").classList.toggle("trans")
+});
+
+document.querySelector(".img-box3").addEventListener("click", ()=>{
+    document.querySelector(".img3").classList.toggle("trans")
+});
+document.querySelector(".img-box4").addEventListener("click", ()=>{
+    document.querySelector(".img4").classList.toggle("trans")
+});
+document.querySelector(".img-box5").addEventListener("click", ()=>{
+    document.querySelector(".img5").classList.toggle("trans")
+});
+document.querySelector(".img-box6").addEventListener("click", ()=>{
+    document.querySelector(".img6").classList.toggle("trans")
+});
+document.querySelector(".img-box7").addEventListener("click", ()=>{
+    document.querySelector(".img7").classList.toggle("trans")
+});
+document.querySelector(".img-box8").addEventListener("click", ()=>{
+    document.querySelector(".img8").classList.toggle("trans")
+});
+document.querySelector(".img-box9").addEventListener("click", ()=>{
+    document.querySelector(".img9").classList.toggle("trans")
+});
+document.querySelector(".img-box10").addEventListener("click", ()=>{
+    document.querySelector(".img10").classList.toggle("trans")
+});
+document.querySelector(".img-box11").addEventListener("click", ()=>{
+    document.querySelector(".img11").classList.toggle("trans")
+});
+document.querySelector(".img-box12").addEventListener("click", ()=>{
+    document.querySelector(".img12").classList.toggle("trans")
+});
+document.querySelector(".img-box13").addEventListener("click", ()=>{
+    document.querySelector(".img13").classList.toggle("trans")
+});
+document.querySelector(".img-box14").addEventListener("click", ()=>{
+    document.querySelector(".img14").classList.toggle("trans")
+});
+document.querySelector(".img-box15").addEventListener("click", ()=>{
+    document.querySelector(".img15").classList.toggle("trans")
+});
+document.querySelector(".img-box16").addEventListener("click", ()=>{
+    document.querySelector(".img16").classList.toggle("trans")
+});
+document.querySelector(".img-box17").addEventListener("click", ()=>{
+    document.querySelector(".img17").classList.toggle("trans")
+});
+document.querySelector(".img-box18").addEventListener("click", ()=>{
+    document.querySelector(".img18").classList.toggle("trans")
+});
+document.querySelector(".img-box19").addEventListener("click", ()=>{
+    document.querySelector(".img19").classList.toggle("trans")
+});
+document.querySelector(".img-box20").addEventListener("click", ()=>{
+    document.querySelector(".img20").classList.toggle("trans")
+});
+document.querySelector(".img-box21").addEventListener("click", ()=>{
+    document.querySelector(".img21").classList.toggle("trans")
+});
+document.querySelector(".img-box22").addEventListener("click", ()=>{
+    document.querySelector(".img22").classList.toggle("trans")
+});
+document.querySelector(".img-box23").addEventListener("click", ()=>{
+    document.querySelector(".img23").classList.toggle("trans")
+});
+document.querySelector(".img-box24").addEventListener("click", ()=>{
+    document.querySelector(".img24").classList.toggle("trans")
+});
